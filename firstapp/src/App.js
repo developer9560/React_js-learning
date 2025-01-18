c099c1d238e8aa8e6198d291469d49e8344b1b6d
developer9560 <developer95608@gmail.com>
2025-01-18 15:52:32 +0530

    understanding about component and header footer

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,17 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './Header';
+import Footer from './Footer';
 function App() {
-  let arr = [12,43,4343,34,342,223,]; 
-  let status = 0;
+ 
   return (
-    <div className="App">
-      <h1>hello suraj kumar</h1>
-      {arr.map((a)=>{
-        return(<div> {a}</div>);// using map print all array's value
-      
-      })};
-     { (status==0)? <h1>this is true</h1>: <h2>this is false</h2>}
+    <div>
+      <Header/>
+      <Footer/>
     </div>
   );
 }
---
+++ firstapp/src/Header.js
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function Header(){
+    return(
+        <div className="headerbox">
+         <nav className="navbox">
+            <ul>
+                <li>Home</li>
+                <li>Services</li>
+                <li>about</li>
+                <li>Contact us</li>
+                <li>SignUp</li>
+            </ul>
+         </nav>
+        </div>
+    );
+}
+
+export default Header;
