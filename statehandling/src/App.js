2a5ee7d80eb72abfcea8635572a1b42bb1e6e5af
developer9560 <developer95608@gmail.com>
2025-01-21 21:42:43 +0530

    creating show and hide password using ternary operator and useState

--- statehandling/src/App.js
+++ statehandling/src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import btnModule from './Button.module.css';
+import Password from './Password.js'
 
 function App() {
   let [status , setstatus] = useState(false);
-  return ( 
+  return (
     <div className="App">
-      <button className={btnModule.error} >click me</button>
+      <Password/>
+      <button className={status ? btnModule.error : btnModule.success}>click me</button>
+       <button className={btnModule.warning} >demo</button>
       <button onClick={() => setstatus(!status)}>
         {status ? "ON" : "OFF"}
         </button>
---
+++ statehandling/src/Password.js
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+export default function Password(){
+    let [status, setStatue] = useState(false);
+
+    
+    return(
+        <div>
+            <h1>password</h1>
+            <input type={status?"text":"password"} placeholder="password" className="password"></input>
+            <button onClick={() => setStatue(!status)}>{status?"Hide":"Show"}</button>
+        </div>
+    )
+}
