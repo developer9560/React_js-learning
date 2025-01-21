7d38fba7dcb78d2f89b0fb4e7f58bfc846de029d
developer9560 <developer95608@gmail.com>
2025-01-22 02:16:56 +0530

    creating FAQs

--- statehandling/src/App.js
+++ statehandling/src/App.js
@@ -4,13 +4,29 @@ import { useState } from 'react';
 import btnModule from './Button.module.css';
 import Password from './Password.js'
 import ResponsiveMenu from './ResponsiveMenu.js';
-
+import faqQuestion from './DATA/faqQuestion.js';
 function App() {
   let [menustatus , setmenustatus] = useState(false);
   let [status , setstatus] = useState(false);
+
+  let[showAsn, setShowAsn] = useState(faqQuestion[0].id);
   return (
     <div className="App">
-      <button className={btnModule.micon} onClick={()=>setmenustatus(!menustatus)} >&#9776;</button>
+      <div>
+        <h1>Frequently Asked Questions (FAQs)</h1>
+        <div className='faqouter'>
+          {faqQuestion.map((v , i)=>{
+            return (
+          <div className='faqItems' key= {i}>    
+            <h3 onClick={()=>setShowAsn(v.id)}>{v.question}</h3>
+            <p className ={showAsn === v.id ? "show" : "hide"}>{v.answer}</p>
+          </div>
+            );
+          })}
+        </div>
+      </div>
+     { /* tthis is previous layout of hide and show password and nevigation bar/}
+      {/* <button className={btnModule.micon} onClick={()=>setmenustatus(!menustatus)} >&#9776;</button>
       <ResponsiveMenu status={menustatus}/>
       <Password/>
       <button className={status ? btnModule.error : btnModule.success}>click me</button>
@@ -19,7 +35,7 @@ function App() {
         {status ? "ON" : "OFF"}
         </button>
       {status ? <h1>hello</h1> : <h1>world</h1>}
-     <p>Welcome to statehandling</p> 
+     <p>Welcome to statehandling</p>  */}
     </div>
   );
 }
